51ccd13c470d8ca3a91143777d32db5ad2c0e178
H B <phajib@gmail.com>
2019-02-03 10:10:25 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,7 @@ def nyc_pigeon_organizer(data)
   data.each do |pigeon_data, value|
   value.each do |data, array|
     array.each do |name|
-      if list.has_key?(name)
+      if list.include?(name)
         if list[name].has_key?(pigeon_data)
           list[name][pigeon_data] << data.to_s
         else
