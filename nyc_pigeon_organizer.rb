0f119377839c0d1a90090cb03d73c3599b108223
H B <phajib@gmail.com>
2019-02-03 10:08:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,7 @@ def nyc_pigeon_organizer(data)
     array.each do |name|
       if list.has_key?(name)
         if list[name].has_key?(pigeon_data)
-          list[name][pigeon_data] << data
+          list[name][pigeon_data] << data.to_s
         else
           list[name][pigeon_data] = [data]
         end
