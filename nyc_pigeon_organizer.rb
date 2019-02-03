75ad5feaa17e9f9647f67beb4b0077fd13e41d96
H B <phajib@gmail.com>
2019-02-03 09:25:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,15 +1,15 @@
 def nyc_pigeon_organizer(data)
   list = {}
   data.each do |pigeon_cat, values|
-    values.each do |name|
+    values.each do |data, array|
       if list.has_key?(name)
-          if pigeon_list[name].has_key?(key)
-            pigeon_list[name][key] << describe.to_s
+          if list[name].has_key?(pigeon_cat)
+            list[name][pigeon_cat] << describe.to_s
           else
-            pigeon_list[name][key] = [describe.to_s]
+            list[name][pigeon_cat] = [data.to_s]
           end
         else
-          pigeon_list[name] = {key => [describe.to_s]}
+          list[name] = {pigeon_cat => [data.to_s]}
         end
       end
     end
